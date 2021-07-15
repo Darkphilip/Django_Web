d4c0986012448d3034880c6350a52033327182fb
tigeryoon <yoonlucky0@gmail.com>
2021-07-15 17:56:11 +0900

    CBV Blog Post list

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.PostList.as_view()),
     path('<int:pk>/', views.single_post_page)
 ]
