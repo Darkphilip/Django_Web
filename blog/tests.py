8328e7e8f837adc3580636460edd2b4afbc22562
tigeryoon <yoonlucky0@gmail.com>
2021-07-19 16:25:54 +0900

    modified post list

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Category
 
 # Create your tests here.
 class TestView(TestCase):
@@ -14,6 +14,31 @@ class TestView(TestCase):
         self.user_better = User.objects.create_user(
             username='better',
             password='anonymous'
+        )
+
+        self.category_programming = Category.objects.create(
+            name='programming', slug='programming'
+        )
+        self.category_music = Category.objects.create(
+            name='music', slug='music'
+        )
+
+        self.post_001 = Post.objects.create(
+            title='첫번째 포스트입니다.',
+            content='Hello, World!. We are the World.',
+            category=self.category_programming,
+            author=self.user_butter
+        )
+        self.post_002 = Post.objects.create(
+            title='두번째 포스트입니다.',
+            content='Django Wine Fresh',
+            category=self.category_music,
+            author=self.user_better
+        )
+        self.post_003 = Post.objects.create(
+            title='세번째 포스트입니다.',
+            content='Category None',
+            author=self.user_better
         )
 
     def navbar_test(self,soup):
@@ -33,62 +58,55 @@ class TestView(TestCase):
         about_me_btn = navbar.find('a', text='About Me')
         self.assertEqual(about_me_btn.attrs['href'], '/about-me')
 
-    def test_post_list(self):
+    def test_post_list_with_posts(self):
         response = self.client.get('/blog/')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertIn('Blog', soup.title.text)
 
         self.navbar_test(soup)
 
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertIn(self.post_002.title, main_area.text)
+        self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+        self.assertIn(self.post_001.author.username.upper(), main_area.text)
+        self.assertIn(self.post_002.author.username.upper(), main_area.text)
+
+    def test_post_list_without_posts(self):
+        Post.objects.all().delete()
         self.assertEqual(Post.objects.count(), 0)
+
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.navbar_test(soup)
+        self.assertIn('Blog', soup.title.text)
+
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다.', main_area.text)
 
-        post_001 = Post.objects.create(
-            title='첫번째 포스트입니다.',
-            content='Hello, World!. We are the World.',
-            author=self.user_butter
-        )
-        post_002 = Post.objects.create(
-            title='두번째 포스트입니다.',
-            content='Django Wine Fresh',
-            author=self.user_better
-        )
-        self.assertEqual(Post.objects.count(),2)
-        response = self.client.get('/blog/')
-        soup = BeautifulSoup(response.content, 'html.parser')
-        main_area = soup.find('div', id='main-area')
-        self.assertIn(post_001.title, main_area.text)
-        self.assertIn(post_002.title, main_area.text)
-        self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
-
-        self.assertIn(post_001.author.username.upper(), main_area.text)
-        self.assertIn(post_002.author.username.upper(), main_area.text)
-
     def test_post_detail(self):
-        post_001 = Post.objects.create(
-            title='첫번째 포스트입니다.',
-            content='Django Jinro Fresh',
-            author=self.user_butter
-        )
-        self.assertEqual(Post.objects.count(), 1)
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
-        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(Post.objects.count(), 3)
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
+
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
         self.navbar_test(soup)
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id="post-area")
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
 
         self.assertIn(self.user_butter.username.upper(), post_area.text)
 
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
 
