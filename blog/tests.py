2ac7b40e803a10225e4f2b13533ef63f7d344a7f
tigeryoon <yoonlucky0@gmail.com>
2021-07-22 15:55:47 +0900

    포스트 상세 페이지 태그 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -30,7 +30,7 @@ class TestView(TestCase):
             name='파이썬 공부', slug='파이썬&공부'
         )
         self.tag_python_eng = Tag.objects.create(
-            name='python study', slug='python&study'
+            name='python', slug='python'
         )
         self.tag_python_hello = Tag.objects.create(
             name='Hello World', slug='hello world'
@@ -265,12 +265,17 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
                 'title': '세번째 포스트를 수정했습니다.',
                 'content': '안녕 세계? 우리는 하나!',
-                'category': self.category_music.pk
+                'category': self.category_music.pk,
+                'tags_str': '파이썬 공부; 한글 태그, some tag'
             },
             follow=True
         )
