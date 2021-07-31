035cd0a06214097baf0615314360060894121569
DarkPhilip <yoonlucky0@gmail.com>
2021-07-31 10:03:21 +0900

    gunicorn 설정하기

--- basic_project/settings.py
+++ basic_project/settings.py
@@ -151,9 +151,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
